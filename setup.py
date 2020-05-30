2692175a58227837650060f634c1d0ec78410317
pengzh <pengzh@ie.cuhk.edu.hk>
2020-05-30 12:13:57 +0800

    add setup. add simulator. add hex.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="kdd-cup20-solution",
+    version="0.0.1",
+    author="pengzhenghao",
+    author_email="pengzh@ie.cuhk.edu.hk",
+    packages=[
+        'kddcup20',
+        'kddcup20.env',
+    ],
+    install_requires=[]
+)
